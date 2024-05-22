c797ead46acf1c70f3fe023d441141d0dc018010
Maria Granell Ruiz <mgranellruiz@gmail.com>
2024-05-22 11:37:36 +0200

    affiliative adlib updated

--- sociality/darting2023/sociality-csi_darting2023.R
+++ sociality/darting2023/sociality-csi_darting2023.R
@@ -3,8 +3,8 @@
 # Date: 21 May 2024
 # Author: mgranellruiz
 # Disclaimer: This script was created follwing two scrips created by Josefien Tankink
-# Goal: Calculate centrality of individuals in the social network. That is NOT about DSI or particular friendships.
-# ultimately find a single number (or two) that represents the social integration of an individual in a group.
+# Goal: Calculate centrality of IDIndividuals in the social network. That is NOT about DSI or particular friendships.
+# ultimately find a single number (or two) that represents the social integration of an IDIndividual in a group.
 # for the darting of 2023 I will take data for the ranking and the sociality vectors from 2022-07-01 until 2023-02-01.
 # for more information check the README of combination darting
 # ---------------
@@ -32,104 +32,128 @@ ss <- social %>%
     # Select the study period of the second darting
     Date > "2022-07-01" & Date < "2023-02-01"
   ) %>%
-  pivot_longer(cols = starts_with("Remarks"), names_to = "RemarksColumn") %>%
   filter(
     # remove affiliative interactions in odd context
-    !grepl("bge|bgs|nge|encounter|bgr|wgc", tolower(value)), # bge
-    !grepl("bge|bgs|nge|encounter|bgr|wgc|bgd", tolower(Context)), # bge in context
-    !grepl("experiment|xp|box|touchscreen|pattern", tolower(value)), # experiments
-    !grepl("baby", tolower(value)), # remove the actions that are towards the babies
+    # during experiments or presentations, during bge and when the afffiliative is towards the baby
+    !grepl("BGE|Experiment|Babies", Context),
+    !grepl("corn|swab|presentation|experiment|macaroni|bge|box|novel|tolerance", tolower(OtherContext)),
 
     # remove erors in the behaviours that were entered
-    !grepl("grass|place", BehaviourIndividual1),
-    !grepl("griru|pll|bagr", BehaviourIndividual2)
+    #!grepl("grass|place", BehaviourIndiv1),
+    #!grepl("griru|pll|bagr", BehaviourIndiv2)
   ) %>%
-  pivot_wider(names_from = "RemarksColumn", values_from = "value") %>%
   mutate(Obs.nr = row_number(), # create an observation nr
-         BehaviourIndividual1 = tolower(BehaviourIndividual1),
-         BehaviourIndividual2 = tolower(BehaviourIndividual2))
+         BehaviourIndiv1 = tolower(BehaviourIndiv1), # make sure all behaviours are in lower case
+         BehaviourIndiv2 = tolower(BehaviourIndiv2))
 
 # data cleaning -------
-# the unk male is kom, Ratel or naal
-{ss[grepl("kom", ss$RemarksA) & grepl("Unknown", ss$Individual1), "Individual1"] <- "Kom"
-ss[grepl("kom", ss$RemarksA) & grepl("Unknown", ss$Individual2), "Individual2"] <- "Kom"
-ss[grepl("Ratel", ss$RemarksA) & grepl("Unknown", ss$Individual1), "Individual1"] <- "Ratel"
-ss[grepl("naal", ss$RemarksA) & grepl("Unknown", ss$Individual1), "Individual1"] <- "Naal"}
+# the unk male is Xin, Xai or Apa
+{ss[grepl("Xin", ss$OtherContext) & grepl("UnkAM", ss$IDIndividual2), "IDIDIndividual2"] <- "Xin"
+ss[grepl("UnkAM is Xai", ss$Remarks) & grepl("UnkAm", ss$IDIndividual1), "IDIDIndividual1"] <- "Xai"
+ss[grepl("unkAM is apa", ss$Remarks) & grepl("UnkAM", ss$IDIndividual1), "IDIDIndividual1"] <- "Apa"}
 
 # Investigate the remarks and remove the ones that are uncertain
 #a <- as.data.frame(unique(s$RemarksE)) The ones that are left are ok
 
 # merging all the behaviours -----------
-{
-s12 <- ss %>% dplyr::select("Group","Individual1","BehaviourIndividual1","Individual2","BehaviourIndividual2", "Date", "Time", "Obs.nr") %>%
+
+{s12 <- ss %>% dplyr::select("Group","IDIndividual1","BehaviourIndiv1","IDIndividual2","BehaviourIndiv2", "Date", "Time", "Obs.nr") %>%
   mutate(IntOrder = "Interaction1-2")
-s34 <- ss %>% mutate(IntOrder = "Interaction3-4",
-         Individual1 = Individual3,
-         Individual2 = Individual4,
-         BehaviourIndividual1 = BehaviourIndividual3,
-         BehaviourIndividual2 = BehaviourIndividual4
-  ) %>% dplyr::select("Group","Individual1","BehaviourIndividual1","Individual2","BehaviourIndividual2", "Date", "Time", "Obs.nr","IntOrder")
-s56 <- ss %>% mutate(IntOrder = "Interaction5-6",
-         Individual1 = Individual5,
-         Individual2 = Individual6,
-         BehaviourIndividual1 = BehaviourIndividual5,
-         BehaviourIndividual2 = BehaviourIndividual6
-  ) %>% dplyr::select("Group","Individual1","BehaviourIndividual1","Individual2","BehaviourIndividual2", "Date", "Time", "Obs.nr","IntOrder")
+s34 <- ss %>% dplyr::select(-IDIndividual1, -IDIndividual2, -BehaviourIndiv1, -BehaviourIndiv2) %>%
+  mutate(IntOrder = "Interaction3-4",
+         IDIndividual1 = IDIndividual3,
+         IDIndividual2 = IDIndividual4,
+         BehaviourIndiv1 = BehaviourIndiv3,
+         BehaviourIndiv2 = BehaviourIndiv4
+  ) %>% dplyr::select("Group","IDIndividual1","BehaviourIndiv1","IDIndividual2","BehaviourIndiv2", "Date", "Time", "Obs.nr","IntOrder")
+s56 <- ss %>% dplyr::select(-IDIndividual1, -IDIndividual2, -BehaviourIndiv1, -BehaviourIndiv2) %>%
+  mutate(IntOrder = "Interaction5-6",
+         IDIndividual1 = IDIndividual5,
+         IDIndividual2 = IDIndividual6,
+         BehaviourIndiv1 = BehaviourIndiv5,
+         BehaviourIndiv2 = BehaviourIndiv6
+  ) %>% dplyr::select("Group","IDIndividual1","BehaviourIndiv1","IDIndividual2","BehaviourIndiv2", "Date", "Time", "Obs.nr","IntOrder")
 
 ss <- s12 %>% rbind(s34,s56)
 rm(s12,s34,s56)}
 
-# Split behaviours into rows -------
-ss <- split_behaviours(ss, c("BehaviourIndividual1", "BehaviourIndividual2"), ' ')
-ss <- split_behaviours(ss, c("BehaviourIndividual1", "BehaviourIndividual2"), '.')
+# Remove rows in where both behaviours are empty
+ss <- ss %>%
+  mutate(across(-Obs.nr, ~ na_if(., ""))) %>% # make all empty entries NA
+  filter(!(is.na(BehaviourIndiv2) & is.na(BehaviourIndiv1)), # if there is no behaviour for any of them
+         !is.na(IDIndividual1), !is.na(IDIndividual2)) # if one of them is not entered
 
-ss <- ss%>% filter (
-  grepl("gr|sg|sw|em|pl", BehaviourIndividual1), # bge
-  grepl("gr|sg|sw|em|pl", BehaviourIndividual2)
-)
+# Split behaviours into rows ------- warnings are fine
+ss <- suppressWarnings(split_behaviours(ss, c("BehaviourIndiv1", "BehaviourIndiv2"), ' '))
+ss <- suppressWarnings(split_behaviours(ss, c("BehaviourIndiv1", "BehaviourIndiv2"), '.'))
+
+# select behaviours to calculate the sociality
+ss <- ss %>%
+  # make all behaviours into lower case
+  mutate(BehaviourIndiv1 = tolower(BehaviourIndiv1),
+         BehaviourIndiv2 = tolower(BehaviourIndiv2)) %>%
+  # make easy behaviour corrections when one of the behaviours is empty
+  mutate(
+    BehaviourIndiv1 = ifelse(is.na(BehaviourIndiv1) & BehaviourIndiv2 == "bgr", "gr", BehaviourIndiv1),
+    BehaviourIndiv1 = ifelse(is.na(BehaviourIndiv1) & BehaviourIndiv2 == "sg", "sg", BehaviourIndiv1),
+    BehaviourIndiv2 = ifelse(is.na(BehaviourIndiv2) & BehaviourIndiv1 == "gr", "bgr", BehaviourIndiv2),
+    BehaviourIndiv2 = ifelse(is.na(BehaviourIndiv2) & BehaviourIndiv1 == "bgr", "gr", BehaviourIndiv2),
+    BehaviourIndiv2 = ifelse(is.na(BehaviourIndiv2) & BehaviourIndiv1 == "pl", "pl", BehaviourIndiv2),
+    BehaviourIndiv2 = ifelse(is.na(BehaviourIndiv2) & BehaviourIndiv1 == "sg", "sg", BehaviourIndiv2),
+  ) %>%
+  filter (grepl("gr|sg|sw|em|pl", BehaviourIndiv1) | grepl("gr|sg|sw|em|pl", BehaviourIndiv2))
+
 
 # Create actors and recievers ----
-# select only the active behaviours
+# because of the behaviours that we selected sometimes there is no directionality
+# for instance pl or sg. Since we are going to use undirected networks to caluclate sociality
+# it dosen´t matter much, that is as long as they interacted we counted. Regardless of the direction.
+
+# select only the affiliative active behaviours
 active_beh <- c('gr', 'sg', 'sw', 'em', 'pl')
 
-# determine actors ID
-for(i in seq_along(ss$BehaviourIndividual1)) {
-  if(ss$BehaviourIndividual1[i] %in% active_beh) {
-    ss$Actor[i] <- ss$Individual1[i]
-    ss$Receiver[i] <- ss$Individual2[i]
+ssa <- ss
+# determine actors ID.
+for(i in seq_along(ss$BehaviourIndiv1)) {
+  if(ss$BehaviourIndiv1[i] %in% active_beh) {
+    ss$Actor[i] <- ss$IDIndividual1[i]
+    ss$Receiver[i] <- ss$IDIndividual2[i]
   } else {
-    ss$Actor[i] <- ss$Individual2[i]
-    ss$Receiver[i] <- ss$Individual1[i]
+    ss$Actor[i] <- ss$IDIndividual2[i]
+    ss$Receiver[i] <- ss$IDIndividual1[i]
   }
 }
 
 # determine actor behaviour
-for(i in seq_along(ss$BehaviourIndividual1)) {
-  if(ss$Individual1[i] == ss$Actor[i]) {
-    ss$BehaviourActor[i] <- ss$BehaviourIndividual1[i]
+for(i in seq_along(ss$BehaviourIndiv1)) {
+  if(ss$IDIndividual1[i] == ss$Actor[i]) {
+    ss$BehaviourActor[i] <- ss$BehaviourIndiv1[i]
   } else {
-    ss$BehaviourActor[i] <- ss$BehaviourIndividual2[i]
+    ss$BehaviourActor[i] <- ss$BehaviourIndiv2[i]
   }
 }
-rm(i, active_beh)
+rm(i)
 ss <- ss %>% distinct() %>%
-  dplyr::select(-matches("BehaviourIndividual|Individual", ignore.case = FALSE)) %>%
-  filter(!grepl("baby|unknown", tolower(Actor)),
-         !grepl("baby|unknown", tolower(Receiver)))
+  dplyr::select(-matches("BehaviourIndiv|IDIndividual", ignore.case = FALSE)) %>%
+  # remove interactions in where babies or Unk individuals are involved
+  filter(!grepl("BB|Unk", Actor),
+         !grepl("BB|Unk", Receiver)) %>%
+  # remove interactions that are not in the affiliative behaviours
+  filter(BehaviourActor %in% active_beh)
 
-#unique(s$BehaviourActor)
+unique(ssa$BehaviourActor)
 
 #### CREATING *SCANS* CSI FILE ####
 
 # scans data -------------------
-scans <- read.csv('/Users/mariagranell/Repositories/elo-sociality/data/Scan_10.2021-05.2022.csv')
+scans <- read.csv('/Users/mariagranell/Repositories/elo-sociality/data/darting2023/combinedScan_2022-06_2023-5.csv')
 
 # Select the study period of the first darting
 scans <- scans %>%
   mutate(Date = as.character(mdy(Date))) %>%
   filter(Date > "2021-10-01" & Date < "2022-06-01",
          !is.na(Time),
-         !str_detect(Individual, "Unk|Baby")) %>%
+         !str_detect(IDIndividual, "Unk|Baby")) %>%
   mutate(Group = case_when( # there were some errors in the names
       Group == "Noah" ~ "Noha",
       Group == "Lemon Trmp.kept i.p" ~ "Lemon Tree",
@@ -196,7 +220,7 @@ for(i in seq_along(sc$Date)){
 }
 rm(time0, time1, timeinterval,obs.nr,i)
 
-# separate observed individuals
+# separate observed IDIndividuals
 cols <- c("AdultsIn1m", "JuvsIn1m", "AdultsIn2m", "JuvsIn2m", "AdultsIn5m","JuvsIn5m")
 sc <- sc %>% split_behaviours(cols, ";")%>% distinct()
 
@@ -205,11 +229,11 @@ cols <- c("AdultsIn1m", "JuvsIn1m", "AdultsIn2m", "JuvsIn2m", "AdultsIn5m","Juvs
 sc <- sc %>% pivot_longer(cols, values_to = "neighbour", names_to = "distance") %>%
   distinct()%>% filter(!is.na(neighbour), !str_detect( neighbour, "Unk|Baby|adult")) %>%
   rowwise() %>%
-  mutate(dyad = paste(sort(c(Individual, neighbour)), collapse = '-'))
+  mutate(dyad = paste(sort(c(IDIndividual, neighbour)), collapse = '-'))
 rm(cols)
 
 # Remove duplicates
-# If two individuals are scanned within 10 mins of each other both and both have the same network
+# If two IDIndividuals are scanned within 10 mins of each other both and both have the same network
 # is important to one of the observations beacuse they are not independent
 sc <- sc %>%
   group_by(Obs.nr, dyad) %>%
@@ -221,7 +245,7 @@ sc <- sc %>%
 # A file with behaviour. That contains the collumns: Date, Actor, Reciever and Behaviour
 # scans data can be modified in where proximity can be describes as a behaviour. i.e. proox1m, prox5m...
 
-# it dosent include presence data. worryiing? not sure
+# it dosent include presence data. worrying? not sure
 #install.packages("remotes")
 #remotes::install_github("gobbios/socialindices")
 ?socialindices2::CSI()
@@ -231,8 +255,8 @@ library(socialindices2)
 
 # changing scans data to have Date, Actor, Reciever and Behaviour
 sca <- sc %>% mutate(Source = "scan") %>%
-  dplyr::select(Group, Date, Time, Obs.nr, Individual, neighbour, distance, Source) %>%
-  rename(Actor = Individual,
+  dplyr::select(Group, Date, Time, Obs.nr, IDIndividual, neighbour, distance, Source) %>%
+  rename(Actor = IDIndividual,
          Receiver = neighbour,
          Behaviour = distance
   )
